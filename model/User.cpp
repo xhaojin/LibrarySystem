840490cd4e901e655576dad2ba227adddf393023
JinXianhao <2316407231@qq.com>
2026-07-29 08:52:27 +0800

    更新了User实体类，适配MySQL版本

--- model/User.cpp
+++ model/User.cpp
@@ -1,44 +1,132 @@
-#include "model/User.h"
+#include "User.h"
 
 User::User(
-	int id,
-	const std::string& name,
-	Gender gender,
-	int age,
-	const std::string& phone,
-	const std::string& username,
-	const std::string& password,
-	Role role)
-	: id(id), name(name), gender(gender), age(age), phone(phone), username(username), password(password), role(role) {
+    int id,
+    std::string username,
+    std::string password,
+    std::string name,
+    Gender gender,
+    int age,
+    std::string phone,
+    bool enabled,
+    bool deleted)
+    : m_id(id),
+    m_username(std::move(username)),
+    m_password(std::move(password)),
+    m_name(std::move(name)),
+    m_gender(gender),
+    m_age(age),
+    m_phone(std::move(phone)),
+    m_enabled(enabled),
+    m_deleted(deleted)
+{
 }
 
-int User::getId() const {
-	return this->id;
-}
-const std::string& User::getName() const {
-	return this->name;
-}
-const Gender& User::getGender() const {
-	return this->gender;
-}
-int User::getAge() const {
-	return this->age;
-}
-const std::string& User::getPhone() const {
-	return this->phone;
-}
-const std::string& User::getUsername() const {
-	return this->username;
+//================ Getter =================
+
+int User::getId() const
+{
+    return m_id;
 }
 
-const std::string& User::getPassword() const {
-	return this->password;
+const std::string& User::getUsername() const
+{
+    return m_username;
 }
 
-Role User::getRole() const {
-	return this->role;
+const std::string& User::getPassword() const
+{
+    return m_password;
 }
 
-bool User::isAdmin() const {
-	return this->role == Role::Admin;
+const std::string& User::getName() const
+{
+    return m_name;
+}
+
+Gender User::getGender() const
+{
+    return m_gender;
+}
+
+int User::getAge() const
+{
+    return m_age;
+}
+
+const std::string& User::getPhone() const
+{
+    return m_phone;
+}
+
+bool User::isEnabled() const
+{
+    return m_enabled;
+}
+
+bool User::isDeleted() const
+{
+    return m_deleted;
+}
+
+const std::string& User::getLastLoginIp() const
+{
+    return m_lastLoginIp;
+}
+
+const std::optional<std::string>& User::getLastLoginTime() const
+{
+    return m_lastLoginTime;
+}
+
+//================ Setter =================
+
+void User::setUsername(const std::string& username)
+{
+    m_username = username;
+}
+
+void User::setPassword(const std::string& password)
+{
+    m_password = password;
+}
+
+void User::setName(const std::string& name)
+{
+    m_name = name;
+}
+
+void User::setGender(Gender gender)
+{
+    m_gender = gender;
+}
+
+void User::setAge(int age)
+{
+    m_age = age;
+}
+
+void User::setPhone(const std::string& phone)
+{
+    m_phone = phone;
+}
+
+void User::setEnabled(bool enabled)
+{
+    m_enabled = enabled;
+}
+
+void User::setDeleted(bool deleted)
+{
+    m_deleted = deleted;
+}
+
+void User::setLastLoginIp(const std::string& ip)
+{
+    m_lastLoginIp = ip;
+}
+
+void User::setLastLoginTime(const std::optional<std::string>& time)
+{
+    m_lastLoginTime = time;
 }
--- model/User.h
+++ model/User.h
@@ -1,49 +1,97 @@
 #pragma once
 
 #include <string>
-#include <vector>
-#include <algorithm>
-#include <iostream>
-#include "model/Role.h"
+#include <optional>
 
 enum class Gender
 {
-	Male,
-	Female
+    Unknown = 0,
+    Male = 1,
+    Female = 2
 };
 
 class User
 {
-private:
-	int id;
-	std::string name;
-	Gender gender;
-	int age;
-	std::string phone;
-
-	std::string username;
-	std::string password;
-
-	Role role;
-
 public:
-	User(int id, const std::string& name, Gender gender, int age, const std::string& phone, const std::string& username, const std::string& password, Role role);
+    User(
+        int id,
+        std::string username,
+        std::string password,
+        std::string name,
+        Gender gender,
+        int age,
+        std::string phone,
+        bool enabled = true,
+        bool deleted = false);
 
-	int getId() const;
+    User() = default;
 
-	const std::string& getName() const;
+    // Getter
+    int getId() const;
 
-	const Gender& getGender() const;
+    const std::string& getUsername() const;
 
-	int getAge() const;
+    const std::string& getPassword() const;
 
-	const std::string& getPhone() const;
+    const std::string& getName() const;
 
-	const std::string& getUsername() const;
+    Gender getGender() const;
 
-	const std::string& getPassword() const;
+    int getAge() const;
 
-	Role getRole() const;
+    const std::string& getPhone() const;
 
-	bool isAdmin() const;
+    bool isEnabled() const;
+
+    bool isDeleted() const;
+
+    const std::string& getLastLoginIp() const;
+
+    const std::optional<std::string>& getLastLoginTime() const;
+
+    // Setter
+    void setId(int id);
+
+    void setUsername(const std::string& username);
+
+    void setPassword(const std::string& password);
+
+    void setName(const std::string& name);
+
+    void setGender(Gender gender);
+
+    void setAge(int age);
+
+    void setPhone(const std::string& phone);
+
+    void setEnabled(bool enabled);
+
+    void setDeleted(bool deleted);
+
+    void setLastLoginIp(const std::string& ip);
+
+    void setLastLoginTime(const std::optional<std::string>& time);
+
+private:
+    int m_id;
+
+    std::string m_username;
+
+    std::string m_password;
+
+    std::string m_name;
+
+    Gender m_gender;
+
+    int m_age;
+
+    std::string m_phone;
+
+    bool m_enabled;
+
+    bool m_deleted;
+
+    std::string m_lastLoginIp;
+
+    std::optional<std::string> m_lastLoginTime;
 };
