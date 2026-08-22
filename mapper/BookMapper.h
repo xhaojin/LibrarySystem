b18fa3fd21c121566346e51b1e0f51d6dda0aa1f
JinXianhao <2316407231@qq.com>
2026-08-22 19:37:57 +0800

    彻底清楚了SQLite，完成了MySQL Version 1

--- mapper/BookMapper.h
+++ mapper/BookMapper.h
@@ -6,25 +6,10 @@
 
 #include <mysql/jdbc.h>
 
-// SQLite版本（已弃用）
-// #include <QSqlQuery>
-
 //数据库(ResultSet) ↔ Model(Book)
 
 class BookMapper
 {
 public:
-    //==========================
-    // MySQL
-    //==========================
-    
     static std::shared_ptr<Book> fromResultSet(sql::ResultSet& rs);
-
-    //==========================
-    // SQLite（保留接口，已弃用）
-    //==========================
-
-    //static std::shared_ptr<Book> fromQuery(const QSqlQuery& query);
-
-    //static void bindToQuery(QSqlQuery& query,const Book& book);
 };
--- mapper/UserMapper.h
+++ mapper/UserMapper.h
@@ -6,25 +6,10 @@
 
 #include <mysql/jdbc.h>
 
-// SQLite版本（已弃用）
-// #include <QSqlQuery>
-
 //数据库(ResultSet) ↔ Model(User)
 
 class UserMapper
 {
 public:
-    //==========================
-    // MySQL
-    //==========================
-
     static std::shared_ptr<User> fromResultSet(sql::ResultSet& rs);
-
-    //==========================
-    // SQLite（保留接口，已弃用）
-    //==========================
-
-    //static std::shared_ptr<User> fromQuery(const QSqlQuery& query);
-
-    //static void bindToQuery(QSqlQuery& query, const User& user);
 };
